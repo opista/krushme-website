1afba25defb33e487a9198052aebe456bad1ef15
metacurb <10343831+metacurb@users.noreply.github.com>
2026-01-02 15:19:48 +0000

    fix time component ssr?

--- src/components/time.tsx
+++ src/components/time.tsx
@@ -4,15 +4,22 @@ import { DateTime } from "luxon";
 import { useEffect, useState } from "react";
 
 export const Time = () => {
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState<Date | null>(null);
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
+    setMounted(true);
+    setDate(new Date());
     const interval = setInterval(() => {
       setDate(new Date());
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
+
+  if (!mounted || !date) {
+    return <span className="w-20" />;
+  }
 
   return (
     <>{DateTime.fromJSDate(date).setZone("Europe/London").toFormat("h:mm a")}</>

da565c28d3b68c880a0e06f86bc3704443e9ae5e
BeauAgst <10343831+metacurb@users.noreply.github.com>
2026-01-05 11:12:40 +0000

    re-implements revalidate endpoint

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,18 @@
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const secret = searchParams.get("secret");
+
+  if (!secret || secret !== process.env.REVALIDATE_SECRET) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    revalidatePath("/");
+    return NextResponse.json({ success: true });
+  } catch (err) {
+    return NextResponse.json({ error: "Error revalidating" }, { status: 500 });
+  }
+}
